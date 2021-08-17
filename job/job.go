d944a2920dd78e2a4688b3532f0b891ed709af05
Valeri Stanchev <stan4ev6@gmail.com>
2021-08-17 14:32:52 +0300

    add methods to Job

--- job/job.go
+++ job/job.go
@@ -1,15 +1,41 @@
 package job
 
 type Job struct {
-	RunOnce bool
-	Period  int // in seconds
-	Func    func()
+	RunOnce    bool
+	Period     int // in seconds
+	TimeLeft   int
+	PendingRun bool
+	Func       func()
 }
 
 func CreateJob(RunOnce bool, Period int, Func func()) *Job {
-	return &Job{RunOnce: RunOnce, Period: Period, Func: Func}
+	return &Job{RunOnce: RunOnce,
+		Period:     Period,
+		TimeLeft:   Period,
+		PendingRun: (Period == 0),
+		Func:       Func,
+	}
+}
+
+func (job *Job) ShouldRun() bool {
+	return job.PendingRun
+}
+
+func (job *Job) DecreaseTime() {
+	job.TimeLeft -= 1
+}
+
+func (job *Job) ResetTime() bool {
+	if job.RunOnce == true {
+		return false
+	}
+
+	job.TimeLeft = job.Period
+	job.PendingRun = (job.Period == 0)
+	return true
 }
 
 func (job *Job) Run() {
-	job.Func()
+	go job.Func()
+	go job.ResetTime()
 }
